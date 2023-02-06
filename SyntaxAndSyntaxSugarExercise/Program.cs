a2c2e81771b0720cb84fc1ff77846de22cb24c5a
Richard Morris <richmorris55@yahoo.com>
2023-02-06 11:55:48 -0800

    InitialCommit

--- SyntaxAndSyntaxSugarExercise/Program.cs
+++ SyntaxAndSyntaxSugarExercise/Program.cs
@@ -4,7 +4,46 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, Hope you are having a great Monday! Below is todays exercise");
+
+
+            //int answer = 4;
+            //string response;
+            //if (answer < 9)
+            //{
+            //response = answer + " is less than nine";
+            //}
+            //else
+            //{
+            //response = answer + " is greater than or equal to nine";
+            //}
+
+
+
+            //Inferred Typing, String Interpolation, and Tenary Operator 
+
+            Console.WriteLine("Please pick a number between 1 and 20."); 
+
+            var answer = int.Parse(Console.ReadLine());
+
+            var response = (answer < 9) ? $"{answer} is less than nine." : $" {answer} is greater or equal to 9.";
+
+            Console.WriteLine(response);
+
+
+
+            
+
+
+
+
+
+
+
+
+
         }
+
+
     }
 }
